b995951d001ee1a57ac610109b438c8fb7498e0d
chamarairesh1982 <chamara.iresg@gmail.com>
2026-04-03 17:00:42 +0100

    #MP-34-Refactor Unit test

--- tests/ReviewPortal.IntegrationTests/UnitTest1.cs
+++ tests/ReviewPortal.IntegrationTests/UnitTest1.cs
@@ -8,23 +8,4 @@ public class UnitTest1
 
     }
 
-    [Fact]
-    public void Test2()
-    {
-
-    }
-
-    [Fact]
-    public void Test3()
-    {
-
-    }
-
-    [Fact]
-    public void Test4()
-    {
-
-    }
-
-
 }
